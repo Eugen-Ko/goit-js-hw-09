9f62f943cdf020d13abb20fbb88db5ae8b6a820f
Eugen-Ko <kovalchuk.eak@gmail.com>
2021-11-19 14:44:20 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -14,13 +14,30 @@ const stopBtn = document.querySelector('[data-stop]');
 
 // --- Блок обработки -----------------------
 // ------------------------------------------
+
+// Переменная таймера в глобальной зоне видимости
+let timerId = null;
+
+// Функция смены цвета
+const changeColor = () => bodyArea.style.backgroundColor = getRandomHexColor();
+
+// Функция обработки кнопки Старт
 const onStartClick = () => {
+  // вызов changeColor для смены фона на 0й миллисекунде
+  // сразу после нажатия кнопки старт.
   bodyArea.style.backgroundColor = getRandomHexColor();
+  // ----------
   startBtn.setAttribute('disabled', true);
+  timerId = setInterval(changeColor, 1000);
 };
 
+// Функция обработки кнопки Стоп
 const onStopClick = () => {
   startBtn.removeAttribute('disabled');
+  clearInterval(timerId);
+  
+  // Возврат белого цвета фона
+  bodyArea.style.backgroundColor = '#FFFFFF';
 };
 // ------------------------------------------
 
