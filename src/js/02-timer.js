0a4450a7593b22e5a323667d09c9077bdc3b4f84
Eugen-Ko <kovalchuk.eak@gmail.com>
2021-11-19 16:06:28 +0200

    11

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,33 @@
+// ------------------------------------------
+// Импорт библиотек
+// ------------------------------------------
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.2.min.css";
 
 // --- Блок инициализации -------------------
 // ------------------------------------------
 
+const daysCounter = document.querySelector('[data-day]');
+const hoursCounter = document.querySelector('[data-hours]');
+const minutesCounter = document.querySelector('[data-minutes]');
+const secondsCounter = document.querySelector('[data-seconds]');
+
+const fieldInput = document.querySelector('#datetime-picker');
+
+const startBtn = document.querySelector('[data-start]');
+
+// Кнопка Старт неактивна
+startBtn.setAttribute('disabled', true);
+
+// --- Блок обработки -----------------------
+// ------------------------------------------
+
+// ------------------------------------------
+
+// --- Слушатели ----------------------------
+// ------------------------------------------
+fieldInput.addEventListener('onfocus')
+// ------------------------------------------
