d9e158b800aa605dbeb6487430df9fe0623bfd02
Eugen-Ko <kovalchuk.eak@gmail.com>
2021-11-19 14:27:36 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+// --- Функция генерации случайного цвета ---
+// ------------------------------------------
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+// ------------------------------------------
+
+// --- Блок инициализации -------------------
+// ------------------------------------------
+const bodyArea = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+// ------------------------------------------
+
+// --- Блок обработки -----------------------
+// ------------------------------------------
+const onStartClick = () => {
+  bodyArea.style.backgroundColor = getRandomHexColor();
+  startBtn.setAttribute('disabled', true);
+};
+
+const onStopClick = () => {
+  startBtn.removeAttribute('disabled');
+};
+// ------------------------------------------
+
+// --- Слушатели ----------------------------
+// ------------------------------------------
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+// ------------------------------------------
 
