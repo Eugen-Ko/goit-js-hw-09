4e5adf04c0725edee41a7f282d370ebfa85944ec
Eugen Kovalchuk <kovalchuk.eak@gmail.com>
2021-11-23 10:52:19 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,32 +5,72 @@ import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.2.min.css';
 // ------------------------------------------
 
-// ------------------------------------------
-// --- Блок инициализации -------------------
-// ------------------------------------------
 // Объект расположений ----------------------
-
-// ------------------------------------------
-// --- Конец блока инициализации ------------
+const form = document.querySelector('.form');
+// счетчик нажатий "Create promises"
+let numberClick = 0;
 // ------------------------------------------
 
 // ------------------------------------------
 // --- Блок обработки -----------------------
 // ------------------------------------------
+// Собираем значение полей формы
+const createDataObj = () => {
+  const formData = new FormData(form);
 
-function createPromise(position, delay) {
+  const dataObj = {};
+  formData.forEach((value, name) => {
+    dataObj[name] = Number(value);
+  });
+  return dataObj;
+};
+
+// Создаем проммисы
+const createPromise = ({ index, timeDelay }) => {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setInterval(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ index, timeDelay });
+      }
+      // Reject
+      reject({ index, timeDelay });
+    }, timeDelay);
+  });
+};
+
+// Цикл создания промисов по колличеству
+const cycleForCreate = ({ delay, step, amount }) => {
+  let timeDelay = delay;
+
+  for (let index = 1; index <= amount; index++) {
+    timeDelay += step;
+
+    createPromise({ index, timeDelay })
+      .then(({ index, timeDelay }) => {
+        Notiflix.Notify.success(
+          ` При клике № ${numberClick} ${index}-й промис выполнен за ${timeDelay} мсек !!`,
+        );
+      })
+      .catch(({ index, timeDelay }) => {
+        Notiflix.Notify.failure(
+          ` При клике № ${numberClick} ${index}-й промис откленен за ${timeDelay} мсек !!`,
+        );
+      });
   }
-}
+};
+
+// Обработка слушателя по onSubmit
+const onSubmitCreateP = e => {
+  e.preventDefault();
+  numberClick += 1;
+  cycleForCreate(createDataObj());
+};
 
 // ------------------------------------------
 // --- Конец блока обработки ------------
 // ------------------------------------------
 
-// ------------------------------------------
 // --- Слушатели ----------------------------
-// ------------------------------------------
+form.addEventListener('submit', onSubmitCreateP);
